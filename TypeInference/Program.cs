da51e287061239bba4486c9ddc562844b1e37f45
FraOliv <olivotti.francesco@gmail.com>
2021-04-27 11:03:39 +0200

    C#

---
+++ TypeInference/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TypeInference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float f = 12.5f;
+            //per dichiare un float si deve mettere alla fine f
+            double d = 130.78;
+            decimal dm = 1500.456m;
+            //per dichiarare un decimal si mette m alla fine
+            double ne = 5e-7;
+            // e sta *10 e il numero seguente è l'esponente
+            var s = "type inference";
+            var b = false;
+
+            Console.WriteLine($"f = {f} d = {d} dm = {dm} ne = {ne} s = {s} b = {b}");
+            
+        }
+    }
+}
