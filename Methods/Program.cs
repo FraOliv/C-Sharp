58096048c3decc60c8217753993f16418a918b9a
FraOliv <olivotti.francesco@gmail.com>
2021-04-28 16:30:04 +0200

    Methods and Recursive Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //void significa che il metodo non ritorna un valore
+        {
+            Console.Write("Ciao, Inserisci un numero e il programma provvederà a calcolarne il 'Fattoriale' ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"il fattoriale del numero {n} è {Fattoriale(n)}");
+        }
+
+        //definisco ed inizializzo un metodo ricorsivo per calcolare il fattoriale di un numero
+        static int Fattoriale(int n){
+            //scrivo un test condizionale che interrompa la ricorsione in modo di non far crashare il programma. si dice "caso base"
+            if (n == 0)
+                return 1;
+            return n * Fattoriale(n - 1);
+        }
+    }
+}
